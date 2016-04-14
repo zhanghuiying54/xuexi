6163830c6d5d6eb7e205bf5f4aa9e2772d7d2f24
zhanghuiying <zhanghuiying@mantoto.com>
2016-04-14 15:49:28 +0800

    222

--- hell word/hell word/ViewController.m
+++ hell word/hell word/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.、
+    NSLog(@"wwww");
 }
 
 - (void)didReceiveMemoryWarning {
