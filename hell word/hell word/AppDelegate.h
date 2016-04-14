3499763a7204ce9b4fb5c7896856d8bba92933aa
zhanghuiying <zhanghuiying@mantoto.com>
2016-04-14 15:46:03 +0800

    hello tijiao

---
+++ hell word/hell word/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hell word
+//
+//  Created by 漫涂 on 16/4/14.
+//  Copyright © 2016年 漫涂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hell word/hell word/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hell word
+//
+//  Created by 漫涂 on 16/4/14.
+//  Copyright © 2016年 漫涂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
